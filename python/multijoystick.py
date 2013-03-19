3faed948ea5b79e68c57c862082a0cd40a621a2d
Matt Sowden <zxa243@gmail.com>
2013-03-19 17:05:24 -0300

    if elif elif elif elif elif elif elif.

--- python/multijoystick.py
+++ python/multijoystick.py
@@ -3,9 +3,11 @@
 ###############################################################################
 # Module:   multijoystick.py
 # Created:  2 April 2008
-# Author:   Brian D. Wendt
+# Last Updated: 19 March 2013
+# Author:   Brian D. Wendt, Matt Sowden
 #   http://principialabs.com/
-# Version:  0.4
+#   http://exabase.org/
+# Version:  0.6.1.2.3.BCCC
 # License:  GPLv3
 #   http://www.fsf.org/licensing/
 '''
@@ -20,11 +22,13 @@ Dependencies:
   pyserial - http://pyserial.sourceforge.net/
   pygame   - http://www.pygame.org/
   servo    - http://principialabs.com/arduino-python-4-axis-servo-control/
+  
+Furthermore, I am an egg.
 '''
 ###############################################################################
 
 print "\n================================================"
-print "        Arduino/Python Servo Controller"
+print "        Hey There Everyone!"
 print "================================================"
 
 # Import dependent Python modules
@@ -53,11 +57,11 @@ def handleJoyEvent(e):
         axis = "unknown"
         if (e.dict['axis'] == 0):
             axis = "X"
-        if (e.dict['axis'] == 1):
+        elif (e.dict['axis'] == 1):
             axis = "Y"
-        if (e.dict['axis'] == 3):
+        elif (e.dict['axis'] == 3):
             axis = "Throttle"
-        if (e.dict['axis'] == 2):
+        elif (e.dict['axis'] == 2):
             axis = "Z"
 
         # Convert joystick value to servo position for each axis
@@ -75,19 +79,19 @@ def handleJoyEvent(e):
                 # and send to Arduino over serial connection
                 #servo.move(1, serv)
             # Y Axis
-            if (axis == "Y"):
+            elif (axis == "Y"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv[1] = int(90 + move)
                 #servo.move(2, serv)
             # Z Axis - on NAZA, servo 4
-            if (axis == "Z"):
+            elif (axis == "Z"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv[3] = int(90 + move)
                 #servo.move(4, serv)
             # Throttle - on NAZA, servo 3
-            if (axis == "Throttle"):
+            elif (axis == "Throttle"):
                 pos = e.dict['value']
                 move = round(pos * 90, 0)
                 serv[2] = int(90 + move)
@@ -101,21 +105,21 @@ def handleJoyEvent(e):
             # Set pin 13 LED to HIGH for digital on/off demo
             # servo.move(99, 180)
         # Button 2
-        if (e.dict['button'] == 1):
+        elif (e.dict['button'] == 1):
             print "Button 2 Down"
         # Button 3
-        if (e.dict['button'] == 2):
+        elif (e.dict['button'] == 2):
             print "Button 3 Down, Attitude Mode"
             serv[4] = 95
         # Button 4
-        if (e.dict['button'] == 3):
+        elif (e.dict['button'] == 3):
             print "Button 4 Down"
         # Button 5
-        if (e.dict['button'] == 4):
+        elif (e.dict['button'] == 4):
             print "Button 5 Down, Manual Mode"
             serv[4] = 28
         # Button 6
-        if (e.dict['button'] == 5):
+        elif (e.dict['button'] == 5):
             print "Button 6 Down"
             quit()
 
@@ -127,19 +131,19 @@ def handleJoyEvent(e):
             # Set pin 13 LED to LOW for digital on/off demo
             # servo.move(99, 0)
         # Button 2
-        if (e.dict['button'] == 1):
+        elif (e.dict['button'] == 1):
             print "Button 2 Up"
         # Button 3
-        if (e.dict['button'] == 2):
+        elif (e.dict['button'] == 2):
             print "Button 3 Up"
         # Button 4
-        if (e.dict['button'] == 3):
+        elif (e.dict['button'] == 3):
             print "Button 4 Up"
         # Button 5
-        if (e.dict['button'] == 4):
+        elif (e.dict['button'] == 4):
             print "Button 5 Up"
         # Button 6
-        if (e.dict['button'] == 5):
+        elif (e.dict['button'] == 5):
             print "Button 6 Up"
 
     # Assign actions for Coolie Hat Switch events
@@ -147,14 +151,14 @@ def handleJoyEvent(e):
         if (e.dict['value'][0] == -1):
             print "Hat Left"
             servo.move(4, 0)
-        if (e.dict['value'][0] == 1):
+        elif (e.dict['value'][0] == 1):
             print "Hat Right"
             servo.move(4, 180)
-        if (e.dict['value'][1] == -1):
+        elif (e.dict['value'][1] == -1):
             print "Hat Down"
-        if (e.dict['value'][1] == 1):
+        elif (e.dict['value'][1] == 1):
             print "Hat Up"
-        if (e.dict['value'][0] == 0 and e.dict['value'][1] == 0):
+        elif (e.dict['value'][0] == 0 and e.dict['value'][1] == 0):
             print "Hat Centered"
 		
     else:
